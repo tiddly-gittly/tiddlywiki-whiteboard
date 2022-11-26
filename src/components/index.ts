5af652885bea867247550e8b164f69e7f44c7a95
lin onetwo <linonetwo012@gmail.com>
2022-11-26 23:13:51 +0800

    feat: save cycle

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,9 +4,65 @@ import { App, IAppProps } from './tldraw';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 const Widget = require('$:/plugins/linonetwo/tw-react/widget.js').widget as typeof ReactWidget;
 
+export const SAVE_DEBOUNCE_INTERVAL = 1000;
+
 class TldrawWhiteBoardWidget extends Widget<IAppProps> {
-  reactComponent = App;
-  getProps = () => ({ stateTiddler: this.getAttribute('stateTiddler'), height: this.getAttribute('height'), width: this.getAttribute('width') });
+  public reactComponent = App;
+  public getProps = () => {
+    return {
+      currentTiddler: this.editTitle ?? this.getVariable('currentTiddler'),
+      initialTiddlerText: this.editTitle === undefined ? '' : $tw.wiki.getTiddlerText(this.editTitle),
+      height: this.getAttribute('height'),
+      width: this.getAttribute('width'),
+      saver: {
+        onSave: this.onSave,
+        interval: SAVE_DEBOUNCE_INTERVAL,
+      },
+    };
+  };
+
+  private readonly currentTiddler: string | undefined;
+  editorOperations = {};
+  private editTitle: string | undefined;
+  private editField: string | undefined;
+  private editIndex: string | undefined;
+  private editDefault: string | undefined;
+  private editClass: string | undefined;
+  private editMinHeight: string | undefined;
+  private editTabIndex: string | undefined;
+  private isDisabled: string | undefined;
+  private isFileDropEnabled: boolean | undefined;
+
+  execute() {
+    // Get our editor parameters, like in slate-write
+    /** don't use `this.getVariable('currentTiddler')` otherwise it will overwrite the widget. */
+    this.editTitle = this.getAttribute('tiddler');
+    this.editField = this.getAttribute('field', 'text');
+    this.editIndex = this.getAttribute('index');
+    this.editDefault = this.getAttribute('default');
+    this.editClass = this.getAttribute('class');
+    this.editMinHeight = this.getAttribute('minHeight', '100px');
+    this.editTabIndex = this.getAttribute('tabindex');
+    this.isDisabled = this.getAttribute('disabled', 'no');
+    this.isFileDropEnabled = this.getAttribute('fileDrop', 'no') === 'yes';
+    // Get the default editor element tag and type (textarea or div) (not implemented)
+
+    // Make the child widgets
+    this.makeChildWidgets();
+  }
+
+  private readonly onSave = (newText: string): void => {
+    /** if tiddler field is not filled in, just edit in the memory, don't save */
+    if (this.editTitle === '' || this.editTitle === undefined) {
+      return;
+    }
+    const previousText = $tw.wiki.getTiddlerText(this.editTitle) ?? '';
+    // prevent useless call to addTiddler
+    if (previousText === newText) {
+      return;
+    }
+    $tw.wiki.setText(this.editTitle, undefined, undefined, newText);
+  };
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
--- src/components/tldraw.tsx
+++ src/components/tldraw.tsx
@@ -1,20 +1,57 @@
-import { Tldraw } from '@tldraw/tldraw';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { useState, useEffect } from 'react';
+import { TDDocument, Tldraw, TldrawApp } from '@tldraw/tldraw';
+import useDebouncedCallback from 'beautiful-react-hooks/useDebouncedCallback';
+
 import './tldraw.css';
 
+/** every ms to save */
+const debounceSaveTime = 500;
 export interface IAppProps {
-  height?: string;
   /**
    * Tiddler to contain the serialized JSON component state
    */
-  stateTiddler: string;
+  currentTiddler: string;
+  height?: string;
+  initialTiddlerText?: string;
+  saver: {
+    /** ms about debounce how long between save */
+    interval?: number;
+    onSave: (value: string) => void;
+  };
   width?: string;
 }
 
 export function App(props: IAppProps): JSX.Element {
-  const { height, width } = props;
+  const {
+    height,
+    width,
+    initialTiddlerText,
+    saver: { onSave },
+  } = props;
+  const [tldrawDocument, tldrawDocumentSetter] = useState<TDDocument | undefined>();
+  useEffect(() => {
+    if (initialTiddlerText) {
+      try {
+        tldrawDocumentSetter(JSON.parse(initialTiddlerText) as TDDocument);
+      } catch {}
+    }
+  }, [initialTiddlerText]);
+  const debouncedSaveOnChange = useDebouncedCallback(
+    (app: TldrawApp) => {
+      const newTiddlerText = JSON.stringify(app.document);
+      onSave(newTiddlerText);
+    },
+    [],
+    debounceSaveTime,
+  );
+  const onChange = (app: TldrawApp) => {
+    tldrawDocumentSetter(app.document);
+    debouncedSaveOnChange(app);
+  };
   return (
     <div className="tw-whiteboard-tldraw-container" style={{ height, width }}>
-      <Tldraw />
+      <Tldraw onPersist={onChange} document={tldrawDocument} />
     </div>
   );
 }
