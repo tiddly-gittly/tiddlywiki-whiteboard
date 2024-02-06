d957c86a5b84baeaa2cf25daaed722ca7780d710
linonetwo <linonetwo012@gmail.com>
2024-02-06 23:48:39 +0800

    fix: error when tldraw require localStorage on RN

--- src/tw-whiteboard/widget-loader.js
+++ src/tw-whiteboard/widget-loader.js
@@ -5,9 +5,13 @@
     return;
   }
   // separate the widget from the exports here, so we can skip the require of react code if `!$tw.browser`. Those ts code will error if loaded in the nodejs side.
-  const components = require('$:/plugins/linonetwo/tw-whiteboard/widget.js');
-  const { whiteboard } = components;
-  /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-  exports.whiteboard = whiteboard;
-  exports['edit-whiteboard'] = whiteboard;
+  try {
+    const components = require('$:/plugins/linonetwo/tw-whiteboard/widget.js');
+    const { whiteboard } = components;
+    /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+    exports.whiteboard = whiteboard;
+    exports['edit-whiteboard'] = whiteboard;
+  } catch (error) {
+    console.error('Error loading tw-whiteboard widget', error);
+  }
 })();
