3b1f5cbcf33bae4c15ba04bfdb13ee8322c78f35
lin onetwo <linonetwo012@gmail.com>
2024-03-30 23:44:27 +0800

    feat: buttons to fold and open in default layout

--- src/tw-whiteboard/tldraw/shapes/transclude/component.tsx
+++ src/tw-whiteboard/tldraw/shapes/transclude/component.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable unicorn/no-null */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { useWidget } from '$:/plugins/linonetwo/tw-react/index.js';
+import { ParentWidgetContext, useWidget } from '$:/plugins/linonetwo/tw-react/index.js';
 import { getDefaultColorTheme, useEditor, useIsEditing } from '@tldraw/editor';
 import useDebouncedCallback from 'beautiful-react-hooks/useDebouncedCallback';
-import { ChangeEvent, CSSProperties, useCallback, useMemo, useRef } from 'react';
+import { ChangeEvent, CSSProperties, useCallback, useContext, useMemo, useRef } from 'react';
 import { IParseTreeNode } from 'tiddlywiki';
 
 import { TranscludeShape } from './type';
 import './style.css';
 import { lingo } from 'src/tw-whiteboard/utils/lingo';
+import { renderSVGTiddler } from 'src/tw-whiteboard/utils/renderSVGTiddler';
 import { wrapTiddlerAst } from 'src/tw-whiteboard/utils/wrapTiddlerAst';
 import { useOnToggleFold } from './useOnToggleFold';
 
@@ -44,7 +46,6 @@ export function TranscludeComponent({ shape, isDarkMode }: { isDarkMode: boolean
   const editTitleContainerOnClick = useCallback(() => {
     editTitleInputReference.current?.focus?.();
   }, []);
-  const onToggleFold = useOnToggleFold(shape);
 
   const sharedStyle: CSSProperties = {
     backgroundColor: theme[adjustedColor].solid,
@@ -70,9 +71,40 @@ export function TranscludeComponent({ shape, isDarkMode }: { isDarkMode: boolean
         />
       </div>
       <div className='transclude-shape-component-inner' key='render' style={{ display: isEditing ? 'none' : undefined, ...sharedStyle }}>
-        <h2 onClick={onToggleFold}>{tiddlerTitle}</h2>
+        <h2>{tiddlerTitle}</h2>
         <div ref={transcludeRenderContainerReference} style={{ display: shape.props.folded ? 'none' : undefined }}>Transclude loading...</div>
+        <ShapeViewToolbar shape={shape} />
       </div>
+    </div>
+  );
+}
+
+function ShapeViewToolbar({ shape }: { shape: TranscludeShape }) {
+  const foldIcon = useMemo(() => renderSVGTiddler('$:/core/images/fold-button'), []);
+  const foldText = useMemo(() => $tw.wiki.getTiddlerText('$:/language/Buttons/Fold/Caption'), []);
+  const unfoldIcon = useMemo(() => renderSVGTiddler('$:/core/images/unfold-button'), []);
+  const unfoldText = useMemo(() => $tw.wiki.getTiddlerText('$:/language/Buttons/Unfold/Caption'), []);
+  const onToggleFold = useOnToggleFold(shape);
+  // TODO: change to default layout icon
+  const openInStoryIcon = useMemo(() => renderSVGTiddler('$:/core/images/open-window'), []);
+  const openInStoryText = useMemo(() => lingo('OpenInDefault'), []);
+  const parentWidget = useContext(ParentWidgetContext);
+  const onOpenInStory = useCallback(() => {
+    $tw.wiki.setText('$:/layout', 'text', undefined, '');
+    parentWidget?.dispatchEvent({
+      type: 'tm-navigate',
+      navigateTo: shape.props.title,
+    });
+  }, [parentWidget, shape.props.title]);
+
+  if (!shape.props.title) {
+    return null;
+  }
+
+  return (
+    <div className='shape-view-toolbar-container'>
+      <button dangerouslySetInnerHTML={{ __html: shape.props.folded ? unfoldIcon : foldIcon }} onClick={onToggleFold} title={shape.props.folded ? unfoldText : foldText} />
+      <button dangerouslySetInnerHTML={{ __html: openInStoryIcon }} onClick={onOpenInStory} title={openInStoryText} />
     </div>
   );
 }
